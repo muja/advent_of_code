2d93e7250a43e6b0b321ee62ec09aadc462115d7
Danyel Bayraktar <vcs@danyel.io>
2018-12-01 22:58:18 +0100

    perf(day1): use HashSet to increase speed of solution

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[aoc(day1, part1)]
 pub fn part1(input: &str) -> i32 {
   input.split_whitespace().map(|i| i.parse::<i32>().unwrap()).fold(0, std::ops::Add::add)
@@ -5,13 +7,12 @@ pub fn part1(input: &str) -> i32 {
 
 #[aoc(day1, part2)]
 pub fn part2(input: &str) -> i32 {
-  let mut set = vec![0i32];
+  let mut set = HashSet::new();
+  set.insert(0);
   let mut current = 0;
   input.split_whitespace().map(|i| i.parse::<i32>().unwrap()).cycle().take_while(|i| {
     current += i;
-    let b = !set.contains(&current);
-    set.push(current);
-    b
+    set.insert(current)
   }).last().unwrap(); // to ensure the iterator is consumed
   current
 }
