1f89bdd4b9580c96e64a966072b51a581aa03bcd
Danyel Bayraktar <vcs@danyel.io>
2018-12-01 22:57:22 +0100

    refactor(day1): dont output number of iterations

--- src/day1.rs
+++ src/day1.rs
@@ -7,14 +7,11 @@ pub fn part1(input: &str) -> i32 {
 pub fn part2(input: &str) -> i32 {
   let mut set = vec![0i32];
   let mut current = 0;
-  let mut n = 0i32;
   input.split_whitespace().map(|i| i.parse::<i32>().unwrap()).cycle().take_while(|i| {
-    n += 1;
     current += i;
     let b = !set.contains(&current);
     set.push(current);
     b
   }).last().unwrap(); // to ensure the iterator is consumed
-  println!("{}", n);
   current
 }
