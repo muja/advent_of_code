ef2735442ed588e35cb140d09f4f19a2a845cc1a
Danyel Bayraktar <vcs@danyel.io>
2018-12-02 13:38:32 +0100

    feat(day2): implement solution for part2

--- src/day2.rs
+++ src/day2.rs
@@ -22,3 +22,38 @@ pub fn part1(input: &str) -> i32 {
   }
   twos * threes
 }
+
+#[aoc(day2, part2)]
+pub fn part2(input: &str) -> String {
+  if let Some((s1, _, id)) = find_similar_ids(input) {
+    return s1.chars().enumerate().filter(|(i, _)| *i != id).map(|(_, c)| c).collect::<String>();
+  }
+  panic!("input did not contain two ids differing by only one char");
+}
+
+fn find_similar_ids(input: &str) -> Option<(String, String, usize)> {
+  let mut coll: Vec<String> = vec![];
+  for line in input.lines() {
+    for comp in coll.iter() {
+      if let Some(index) = find_single_difference_id(line, comp) {
+        return Some((line.to_owned(), comp.to_owned(), index));
+      }
+    }
+    coll.push(line.to_owned());
+  }
+  None
+}
+
+fn find_single_difference_id(s1: &str, s2: &str) -> Option<usize> {
+  let mut differing: Option<usize> = None;
+  for (i, (c1, c2)) in s1.chars().zip(s2.chars()).enumerate() {
+    if c1 != c2 {
+      if differing.is_some() {
+        // more than one difference -> wrong pair
+        return None;
+      }
+      differing = Some(i);
+    }
+  }
+  return differing;
+}
