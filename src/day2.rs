b6c3162fae04a8f14ed81cc52c1f776180989b8b
Danyel Bayraktar <vcs@danyel.io>
2018-12-02 14:03:51 +0100

    refactor(day2): rename map to occurrences to make it more self documenting

--- src/day2.rs
+++ src/day2.rs
@@ -5,18 +5,18 @@ pub fn part1(input: &str) -> i32 {
   let mut twos = 0i32;
   let mut threes = 0i32;
   for line in input.lines() {
-    let mut map: HashMap<char, i32> = HashMap::new();
+    let mut occurrences: HashMap<char, i32> = HashMap::new();
     for c in line.chars() {
-      if let Some(old) = map.get_mut(&c) {
+      if let Some(old) = occurrences.get_mut(&c) {
         *old += 1;
       } else {
-        map.insert(c, 1);
+        occurrences.insert(c, 1);
       }
     }
-    if map.values().any(|&e| e == 2) {
+    if occurrences.values().any(|&e| e == 2) {
       twos += 1;
     }
-    if map.values().any(|&e| e == 3) {
+    if occurrences.values().any(|&e| e == 3) {
       threes += 1;
     }
   }
